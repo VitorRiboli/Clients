5508fea318000534eaa0e4a4ff9dd48c4109dba5
vitorriboli <vitorriboli1@gmail.com>
2022-11-26 05:34:48 -0300

    Feat CRUD of Clients Finished

--- src/main/java/com/clientes/vitor/controllers/ClientController.java
+++ src/main/java/com/clientes/vitor/controllers/ClientController.java
@@ -1,16 +1,15 @@
 package com.clientes.vitor.controllers;
 
 import com.clientes.vitor.dto.ClientDTO;
-import com.clientes.vitor.entities.Client;
-import com.clientes.vitor.repositories.ClientRepository;
 import com.clientes.vitor.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Optional;
+import java.net.URI;
 
 @RestController //Configura está classe para responder as Requesições da Web
 @RequestMapping(value = "/clients") //Configura a rota
@@ -19,8 +18,38 @@ public class ClientController {
     @Autowired
     private ClientService service;
 
-    @GetMapping("/{id}")
-    public ClientDTO findById(@PathVariable Long id) {
-        return service.findById(id);
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
 }
--- src/main/java/com/clientes/vitor/services/ClientService.java
+++ src/main/java/com/clientes/vitor/services/ClientService.java
@@ -4,10 +4,13 @@ import com.clientes.vitor.dto.ClientDTO;
 import com.clientes.vitor.entities.Client;
 import com.clientes.vitor.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -19,7 +22,41 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = rep.findById(id).get();
         return new ClientDTO(client);
+    }
 
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> res = rep.findAll(pageable);
+        return res.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = rep.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        Client entity = rep.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = rep.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        rep.deleteById(id);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
     }
 
 }
